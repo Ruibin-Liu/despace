81fbbd5eeff810768b83e27b77e0e6f45932c586
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-18 12:57:48 -0400

    Update __init__.py

---
+++ despace/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Intialization of the package
+#
+# ----------------------------
+
+from despace.spatial_sort import SortND
+
+__all__ = ['SortND']
+__name__ = 'despace'
