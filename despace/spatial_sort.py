2908b8f6c32a07b4ef82a9102ff128347b75ef98
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-18 09:20:57 -0400

    pep8

--- despace/spatial_sort.py
+++ despace/spatial_sort.py
@@ -1,6 +1,10 @@
 import numpy as np
 from math import floor
 import matplotlib.pyplot as plt
+from prompt_toolkit import VERSION
+
+VERSION = '0.1.0'
+
 
 class SortND:
     """
@@ -8,7 +12,7 @@ class SortND:
 
         For the n=1 case, it's just a single sort.
         For the n>=2 dimensional cases, it's possible to index 'coordinates' so that proximal points are stored closely. 
-        
+
         We do it in the following way:
             1) Sort the the n-D coordinates by the first dimension;
             2) Divide the coordinates into two;
@@ -16,10 +20,11 @@ class SortND:
             4) For each sorted half, divide the coordinates into two, and for each half in halves, sort by the next dimension, and so on;
             5) Repeat by circularly rotating the dimension indices until divided to individual elements.
     """
+
     def __init__(self, coords=None, start_dim=0, plot_data=False, **kwargs):
         """
         Init an SortND instance.
-        
+
         params:
             coords: numpy.ndarray like data; default is None.
             start_dim: int, sort the n-D array starting from the `start_dim` dimension; default is 0, the 0th dimension.
@@ -39,10 +44,10 @@ class SortND:
     def _next_dim(self, current_dim):
         """
         Generate the next dimension from the current dimension and the number of dimensions.
-        
+
         params:
             current_dim: int, the current dimension in an array-like object.
-         
+
         returns:
             dim_next: int, the next dimension in a circular way.
         """
@@ -51,11 +56,11 @@ class SortND:
     def _sort_divide(self, coords, dim):
         """
         Sort an array 'coords' by its dimension 'dim', and then divide it into two halves.
-        
+
         params:
             coords: numpy.ndarray-like, array to sort and divide.
             dim: int, the dimension to sort.
-        
+
         returns:
             coords_1: first half of the sorted coords.
             coords_2: second half of the sorted coords.
@@ -68,15 +73,15 @@ class SortND:
         dim_next = self._next_dim(dim)
         coords = coords[coords[:, dim].argsort()]
         half_index = floor(N/2)
-        return self._sort_divide(coords[0:half_index,:], dim_next), self._sort_divide(coords[half_index:N,:], dim_next) 
+        return self._sort_divide(coords[0:half_index, :], dim_next), self._sort_divide(coords[half_index:N, :], dim_next)
 
     def _flatten(self, data):
         """
         Flatten deeply nested tuples into a one-layer tuple of elements.
-        
+
         params:
             data: nested tuples.
-        
+
         returns:
             result: flattened tuple.
         """
@@ -133,7 +138,7 @@ class SortND:
             self.fc = kwargs.get('fc', face_color)
             self.width = kwargs.get('width', 0.002)
             self.head_width = kwargs.get('head_width', 0.02)
-    
+
         if self.d in [1, 2]:
             ax = fig.add_subplot()
             ax.axis('equal')
@@ -144,37 +149,45 @@ class SortND:
             return False
 
         if self.d == 1:
-            ax.scatter(data, data, s=self.scatter_size, c=np.arange(self.length), cmap=self.color_map)
+            ax.scatter(data, data, s=self.scatter_size,
+                       c=np.arange(self.length), cmap=self.color_map)
             if self.plot_arrow:
                 for i in range(self.length-1):
                     dx = data[i+1] - data[i]
                     dy = dx
-                    ax.arrow(data, data, dx, dy, ls=self.ls, fc=self.fc, length_includes_head=True, shape='full', width=self.width, head_width=self.head_width)
+                    ax.arrow(data, data, dx, dy, ls=self.ls, fc=self.fc, length_includes_head=True,
+                             shape='full', width=self.width, head_width=self.head_width)
         elif self.d == 2:
-            ax.scatter(data[:, 0], data[:, 1], s=self.scatter_size, c=np.arange(self.length), cmap=self.color_map)
+            ax.scatter(data[:, 0], data[:, 1], s=self.scatter_size,
+                       c=np.arange(self.length), cmap=self.color_map)
             if self.plot_arrow:
                 for i in range(self.length-1):
                     dx = data[i+1, 0] - data[i, 0]
                     dy = data[i+1, 1] - data[i, 1]
-                    ax.arrow(data[i, 0], data[i, 1], dx, dy, ls=self.ls, fc=self.fc, length_includes_head=True, shape='full', width=self.width, head_width=self.head_width)
+                    ax.arrow(data[i, 0], data[i, 1], dx, dy, ls=self.ls, fc=self.fc,
+                             length_includes_head=True, shape='full', width=self.width, head_width=self.head_width)
         elif self.d == 3:
-            ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=np.arange(self.length), cmap=self.color_map)
+            ax.scatter(data[:, 0], data[:, 1], data[:, 2],
+                       c=np.arange(self.length), cmap=self.color_map)
             if self.plot_arrow:
                 for i in range(self.length-1):
                     dx = data[i+1, 0] - data[i, 0]
                     dy = data[i+1, 1] - data[i, 1]
                     dz = data[i+1, 2] - data[i, 2]
-                    ax.quiver(data[i, 0], data[i, 1], data[i, 2], dx, dy, dz, ls=self.ls, fc=self.fc)
-        
+                    ax.quiver(data[i, 0], data[i, 1], data[i, 2],
+                              dx, dy, dz, ls=self.ls, fc=self.fc)
+
         if self.d != 3:  # The z axis is often cutout in 3D projection plot
             plt.tight_layout()
 
         if self.save_plot:
             self.fig_format = kwargs.get('fig_format', 'png')
             self.transparent = kwargs.get('transparent', True)
-            self.file_name = kwargs.get('file_name', f"{self.d}D_{self.length}.{self.fig_format}")
-            plt.savefig(self.file_name, dpi=self.fig_dpi, transparent=self.transparent)
-    
+            self.file_name = kwargs.get(
+                'file_name', f"{self.d}D_{self.length}.{self.fig_format}")
+            plt.savefig(self.file_name, dpi=self.fig_dpi,
+                        transparent=self.transparent)
+
         if self.show_plot:
             plt.show()
         plt.close()
