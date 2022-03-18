c691d0341dd29c0500d5af775193d104b61b78f0
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-18 15:56:59 -0400

    update license

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
