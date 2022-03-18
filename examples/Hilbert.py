7652574c129df056ebffc7b3c39872c8bf086d6e
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-18 00:37:47 -0400

    fix import

--- examples/Hilbert.py
+++ examples/Hilbert.py
@@ -1,7 +1,12 @@
 """
 Test whether we can reproduce Hilbert curves
 """
+#!/usr/bin/env python3
+
 import sys
+import numpy as np
 
 sys.path.append("..")
-from despace import sort_coordinates as sc
+from despace.spatial_sort import SortND
+
+# TO DO
