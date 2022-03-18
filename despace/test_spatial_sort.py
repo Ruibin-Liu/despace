18e6ee646ca74a2c1c488179deb5d942f6d13e50
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-17 23:58:30 -0400

    test call

--- despace/test_spatial_sort.py
+++ despace/test_spatial_sort.py
@@ -1,5 +1,7 @@
 from spatial_sort import SortND
 import numpy as np
-s = SortND(np.random.rand(40,3))
+s = SortND(np.random.rand(40, 3))
 s.sort()
-s.plot(show_plot=True, plot_arrow=True)
+s.plot(plot_arrow=True)
+s(np.random.rand(50, 2))
+s.plot(show_plot=True)
