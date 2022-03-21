496de4bc6e6206deebcadaa7962075707ef7653c
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-21 10:28:20 -0400

    set up test config

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import sys
+
+
+@pytest.fixture
+def capture_stdout(monkeypatch):
+    buffer = {"stdout": "", "write_calls": 0}
+
+    def fake_write(s):
+        buffer["stdout"] += s
+        buffer["write_calls"] += 1
+
+    monkeypatch.setattr(sys.stdout, 'write', fake_write)
+    return
+
+
+@pytest.fixture(scope="class")
+def init_sortnd():
+    pass
