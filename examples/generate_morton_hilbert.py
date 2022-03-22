78fe99692e21b2b7f05097205e9b1f4aed981eda
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-22 10:20:39 -0400

    morton hilbert example

--- examples/generate_morton_hilbert.py
+++ examples/generate_morton_hilbert.py
@@ -2,14 +2,12 @@
 """
 Construct Hilbert space-filling curves
 """
+from despace.spatial_sort import SortND
 import sys
-import numpy as np
 
 sys.path.append("..")
-from despace.spatial_sort import SortND
 
 s = SortND(sort_type='Hilbert')
 data = [[i, j] for i in range(8) for j in range(8)]
 s.sort(data)
 s.plot(show_plot=False, plot_arrow=True)
-
