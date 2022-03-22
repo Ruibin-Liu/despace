d7cfd44a97c3f06cb6c0b573ec8ec488ac4e45f6
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-21 22:15:52 -0400

    2D hilbert

--- examples/Hilbert.py
+++ examples/Hilbert.py
@@ -1,12 +1,15 @@
-"""
-Test whether we can reproduce Hilbert curves
-"""
 #!/usr/bin/env python3
-
+"""
+Construct Hilbert space-filling curves
+"""
 import sys
 import numpy as np
 
 sys.path.append("..")
 from despace.spatial_sort import SortND
 
-# TO DO
+s = SortND(sort_type='Hilbert')
+data = [[i, j] for i in range(8) for j in range(8)]
+s.sort(data)
+s.plot(show_plot=False, plot_arrow=True)
+
