b37783088b9b4c1fd582a42f7a97549f9c8785d3
Ruibin Liu <ruibinliuphd@gmail.com>
2022-03-22 22:48:00 -0400

    fix pre-commit fails

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ import os
 __package_name__ = "despace"
 __author__ = "Ruibin Liu"
 
-from setuptools import setup, find_packages  # type: ignore
+from setuptools import find_packages, setup  # type: ignore
 
-if os.path.exists('README.md'):
-    long_description = open('README.md').read()
+if os.path.exists("README.md"):
+    long_description = open("README.md").read()
 else:
-    long_description = 'Despace - A python package to sort or index N-dimensional coordinates.'
+    long_description = (
+        "Despace - A python package to sort or index N-dimensional coordinates."
+    )
 
-with open('requirements.txt') as f:
-    REQUIREMENTS = f.read().strip().split('\n')
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().strip().split("\n")
 
-with open('despace/version.py', 'r') as vf:
-    __version__ = vf.readline().split()[-1].strip().replace("'", '')
+with open("despace/version.py", "r") as vf:
+    __version__ = vf.readline().split()[-1].strip().replace("'", "")
 
 
 setup(
@@ -26,9 +28,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Ruibin-Liu/despace",
-    project_urls={
-        "Bug Tracker": "https://github.com/Ruibin-Liu/despace/issues",
-    },
+    project_urls={"Bug Tracker": "https://github.com/Ruibin-Liu/despace/issues"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
